da5c26ca1f9c7e9e751bb014825434d02e5d9333
Mohan-8 <smohanakrishnan82@gmail.com>
2024-11-12 22:52:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+// server/index.js
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const corsOptions = {
+  origin: "*", // Allow only localhost:3000
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type"],
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI; // Replace with your MongoDB URI
+
+// Connect to MongoDB
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+// Define a User schema (adjust based on your user model in MongoDB)
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+});
+
+const User = mongoose.model("User", userSchema);
+app.get("/", (req, res) => {
+  res.send(
+    "WORKING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+  );
+});
+// Define route to get the number of users
+app.get("/api/users/count", async (req, res) => {
+  try {
+    const count = await User.countDocuments();
+    res.json({ count });
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching user count" });
+  }
+});
+
+// Add this new route to get user details
+app.get("/api/users", async (req, res) => {
+  try {
+    const users = await User.find(
+      {},
+      "telegramId firstName lastName rewards referredBy solanaClaimed solanaAddress"
+    );
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching users" });
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
